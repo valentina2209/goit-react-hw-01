982967d396785dbe2a728f57a0960294696ab295
Valya Vovchenko <valya253@ukr.net>
2025-04-17 22:21:42 +0300

    error correction

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,9 +3,11 @@ import css from './FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={css.card}>
-      <span className={isOnline ? css.online : css.offline}></span>
       <img src={avatar} alt="Avatar" width="48" className={css.avatar} />
       <p className={css.name}>{name}</p>
+      {<span className={isOnline ? css.online : css.offline}>
+      {isOnline ? "Online" : "Offline"}
+      </span>}
     </div>
   );
 };
